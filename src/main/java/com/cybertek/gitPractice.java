319b20fd8b5846fa1fccd12d9d5e75794fa5cfda
Nurs77 <nursultan.tolepbergen@gmail.com>
2020-05-21 17:15:24 -0400

    updated

--- src/main/java/com/cybertek/gitPractice.java
+++ src/main/java/com/cybertek/gitPractice.java
@@ -3,5 +3,6 @@ package com.cybertek;
 public class gitPractice {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("new things");
     }
 }
